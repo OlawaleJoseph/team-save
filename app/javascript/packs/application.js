201612b5f069058d32717a0e29f051226d3d0d78
OlawaleJoseph <adedekojoseph@gmail.com>
2020-09-01 17:02:08 +0100

    modify bootstrap setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,14 @@ require("channels")
 
 import toastr from 'toastr'
 window.toastr = toastr
+
+import "../stylesheets/application" 
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
+ 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
