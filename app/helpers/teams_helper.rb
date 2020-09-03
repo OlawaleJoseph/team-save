43fa93e5693d5d38dcf36d0cbfd0762679ba6794
OlawaleJoseph <adedekojoseph@gmail.com>
2020-09-03 22:20:56 +0100

    fix linter errors

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,10 +1,10 @@
 module TeamsHelper
   def display_team_logo(team, expense = nil)
     src = ''
-    src << if (expense && expense.teams.length > 0) && team.image.attached?
-            image_tag(url_for(team.image), class: 'img-fluid custom-card-img')
+    src << if (expense && expense.teams.length.positive?) && team.image.attached?
+             image_tag(url_for(team.image), class: 'img-fluid custom-card-img')
            else
-            image_tag('broken-img.png', class: 'img-fluid custom-card-img')
+             image_tag('broken-img.png', class: 'img-fluid custom-card-img')
            end
     src.html_safe
   end
