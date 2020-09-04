86a465c2803447aa453b7d5fdd32d4a58353526b
OlawaleJoseph <adedekojoseph@gmail.com>
2020-09-04 16:19:25 +0100

    update expense card

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -2,9 +2,9 @@ module TeamsHelper
   def display_team_logo(team, expense = nil)
     src = ''
     src << if (expense && expense.teams.length.positive?) && team.image.attached?
-             image_tag(url_for(team.image), class: 'img-fluid custom-card-img')
+             image_tag(url_for(team.image), class: 'custom-card-img')
            else
-             image_tag('broken-img.png', class: 'img-fluid custom-card-img')
+             image_tag('broken-img.png', class: 'custom-card-img')
            end
     src.html_safe
   end
