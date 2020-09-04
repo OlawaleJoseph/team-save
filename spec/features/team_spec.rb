9832381c7633d0e0d695e793a7349ab90ca16639
OlawaleJoseph <adedekojoseph@gmail.com>
2020-09-04 10:16:15 +0100

    update team test desription

--- spec/features/team_spec.rb
+++ spec/features/team_spec.rb
@@ -10,7 +10,7 @@ RSpec.feature 'Teams', type: :feature do
       expect(page.current_path).to eq(new_session_path)
     end
 
-    scenario 'Validate user input' do
+    scenario 'Should not create a team with invalid input' do
       sign_in person
 
       visit new_team_path
@@ -21,14 +21,14 @@ RSpec.feature 'Teams', type: :feature do
       expect(page.current_path).to eq(teams_path)
     end
 
-    scenario 'valid parameters given' do
+    scenario 'Shoud Create a team' do
       expect { create_team person }.to change { Team.count }.by(1)
       expect(Team.last.creator.id).to eq(person.id)
       expect(page.current_path).to eq('/teams')
       expect(page).to have_content('My Teams')
     end
 
-    scenario 'unique team name' do
+    scenario 'Should not create duplicate teams' do
       create_team person
 
       expect { create_team person }.not_to(change { Team.count })
